4b52ff2f66d3cf9363d43fb1afe98c8480f1e0f0
Dawn Kline <dawn.kline.m@gmail.com>
2023-06-16 14:17:49 -0500

    update profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -54,14 +54,14 @@ const Profile = () => {
             showUsername={false}
           />
         </div>
-        {!userParam && (
+        {/* {!userParam && (
           <div
             className="col-12 col-md-10 mb-3 p-3"
             style={{ border: '1px dotted #1a1a1a' }}
           >
-            <EventForm />
+
           </div>
-        )}
+        )} */}
       </div>
     </div>
   );
