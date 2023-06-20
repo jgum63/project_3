1612f99c0f6ba11ea23ec13f39b0633b9ad1f27b
Jason Tavera <taverajason7@gmail.com>
2023-06-19 23:30:27 -0500

    changed text on homepage and fixed image on my events page

--- client/src/NewComponents/CardSection/Cards.jsx
+++ client/src/NewComponents/CardSection/Cards.jsx
@@ -11,19 +11,19 @@ function Cards() {
           <ul className="cards__items">
             <CardItem
               src="images/img-2.jpg"
-              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              text="move through water in a small water vessel with the aid of a double-bladed paddle"
               path="/events"
             />
             <CardItem
               src="images/img-3.jpg"
-              text="Travel through the Islands of Bali in a Private Cruise"
+              text="Float on the ocean surface whilst exploring the marine life underneath"
               path="/events"
             />
           </ul>
           <ul className="cards__items">
-            <CardItem src="" text="example" path="/events" />
-            <CardItem src="" text="example" path="/events" />
-            <CardItem src="" text="example" path="events" />
+            <CardItem src="images/Beachcombing.png" text="Beachcombing!" path="/events" />
+            <CardItem src="images/Fishing.png" text="Fishing!" path="/events" />
+            <CardItem src="images/Helicopter.png" text="Helicopter Rides!" path="events" />
           </ul>
         </div>
       </div>
